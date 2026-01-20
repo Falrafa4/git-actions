5983b4beb1bdcdd6b08aba2f317d1ba1eb53624b
Fal <rafaasad9@gmail.com>
2026-01-20 10:01:51 +0700

    Add docstrings and fix function call

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+"""
+Simple greeting functions.
+"""
+
 def hello():
+    """Print a greeting message."""
     print("hi")
 
 
 def bye():
+    """Print a farewall message."""
     print("bye")
 
 
-print(hello())
+hello()
